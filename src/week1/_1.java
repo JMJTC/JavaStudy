3dbd3fa314ab6bac834bd452855a7706987b8af7
Jerry <2801987135@qq.com>
2023-10-16 15:58:45 +0800

    week1

--- src/week1/_1.java
+++ src/week1/_1.java
@@ -1,4 +1,15 @@
 package week1;
 
+import java.util.Scanner;
+
 public class _1 {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner=new Scanner(System.in);
+        n=scanner.nextInt();
+        n--;
+        long a=n/3,b=n/5,c=n/15;
+        long sum=a*(a+1)/2*3+b*(b+1)/2*5-c*(c+1)/2*15;
+        System.out.println(sum);
+    }
 }
