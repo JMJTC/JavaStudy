de3d8093fae0e809eb32ce3e8a43d86ebbea6a8c
Jerry <2801987135@qq.com>
2023-10-20 00:03:29 +0800

    8

--- src/week1/_8.java
+++ src/week1/_8.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 class data{
     public long number;
-    public long count;
-    public data(long number,long count){
+    public short count;
+    public data(long number,short count){
         this.number=number;
         this.count=count;
     }
@@ -20,7 +20,7 @@ public class _8 {
         //在声明对象数组后 ，必须对每个数组成员进行实例化话 才能直接使用，否则报空指针异常！
         //初始化
         for(int i=0;i<data.length;i++){
-            data[i]=new data(i+1,1);
+            data[i]=new data(i+1, (short) 1);
         }
         for(int i=0;i<data.length;i++){
 //            if(i==113382)
