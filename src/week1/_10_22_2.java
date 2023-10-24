5b1fb8e436f9954f12c550d96a96d2e1a59cecf9
Jerry <2801987135@qq.com>
2023-10-24 21:44:57 +0800

    10.22

---
+++ src/week1/_10_22_2.java
@@ -0,0 +1,18 @@
+package week1;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class _10_22_2 {
+    public static void main(String[] args){
+        int n;
+        Scanner in=new Scanner(System.in);
+        n= in.nextInt();
+        int sum=0;
+        String num= BigInteger.ONE.shiftLeft(n).toString();
+        for(int i=0;i<num.length();i++){
+            sum+=num.charAt(i)-'0';
+        }
+        System.out.println(sum);
+    }
+}
