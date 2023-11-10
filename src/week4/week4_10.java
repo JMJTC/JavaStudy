b95c70c1d43c5ff88d424ce12bc1d6df6bf7f39f
Jerry <2801987135@qq.com>
2023-11-10 15:15:54 +0800

    修改

--- src/week4/week4_10.java
+++ src/week4/week4_10.java
@@ -6,18 +6,37 @@ public class week4_10 {
         int[] cnt=new int[max];
         int ans=0,n=nums.length;
         //先统计i,j
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cnt[nums[i]&nums[j]]++;
+        for (int num : nums) {
+            for (int i : nums) {
+                cnt[num & i]++;
             }
         }
-        for(int k=0;k<n;k++){
-            for(int ij=0;ij<max;ij++){
-                if((ij&nums[k])==0){
-                    ans+=cnt[ij];
+        for (int num : nums) {
+            for (int ij = 0; ij < max; ij++) {
+                if ((ij & num) == 0) {
+                    ans += cnt[ij];
                 }
             }
         }
+        return ans;
+    }
+
+    public int countTripletsTwo(int[] nums) {
+        int u = 1;
+        for (int x : nums)
+            while (u <= x)
+                u <<= 1;
+        int[] cnt = new int[u];
+        cnt[0] = nums.length; // 直接统计空集
+        for (int m : nums) {
+            m ^= u - 1;
+            for (int s = m; s > 0; s = (s - 1) & m) // 枚举 m 的非空子集
+                ++cnt[s];
+        }
+        int ans = 0;
+        for (int x : nums)
+            for (int y : nums)
+                ans += cnt[x & y];
         return ans;
     }
 }
