d91cc41afdb6f6703e76e88a949b00285fa94666
Jerry <2801987135@qq.com>
2023-11-15 12:25:39 +0800

    修改

--- src/week4/week4_12.java
+++ src/week4/week4_12.java
@@ -6,17 +6,19 @@ import java.util.Scanner;
  * @author jmjtc
  */
 public class week4_12 {
-    public static boolean isHuiWen(int[] a,int[] b){
-        int n=a.length;
-        int[] temp=new int[n];
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            temp[i]=a[i]+b[i];
-            if(temp[i]%2!=0){
-                cnt++;
-            }
-        }
-        return cnt < 2;
+    public static boolean isHuiWen(int a,int b){
+//        int n=a.length;
+//        int[] temp=new int[n];
+//        int cnt=0;
+//        for(int i=0;i<n;i++){
+//            temp[i]=a[i]+b[i];
+//            if(temp[i]%2!=0){
+//                cnt++;
+//            }
+//        }
+//        return cnt < 2;
+        int mask=a^b;
+        return mask==0||(mask&(mask-1))==0;
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
@@ -26,11 +28,11 @@ public class week4_12 {
             data[i]=sc.next();
         }
         //统计字符串中每个字母出现的次数(记录其奇偶性即可)
-        int[][] dataCount=new int[n][26];
+        int[] dataCount=new int[n];
         for(int i=0;i<n;i++){
             int length=data[i].length();
             for(int j=0;j<length;j++){
-                dataCount[i][data[i].charAt(j)-'a']++;
+                dataCount[i]=dataCount[i]^(1<<data[i].charAt(j)-'a');
             }
         }
         int ans=0;
