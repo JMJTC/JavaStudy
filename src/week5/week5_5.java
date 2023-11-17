320bc02cc984ba81322974c66af6615181336090
Jerry <2801987135@qq.com>
2023-11-17 13:33:47 +0800

    修改

--- src/week5/week5_5.java
+++ src/week5/week5_5.java
@@ -1,10 +1,16 @@
 package week5;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class week5_5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    }
 }
 
 
-
+//暴力遍历
 class week5_5_Solution1 {
     public int[] finalPrices(int[] prices) {
         int length=prices.length;
@@ -22,5 +28,25 @@ class week5_5_Solution1 {
 
         }
         return ans;
+    }
+}
+
+//单调栈
+class week5_5_Solution2{
+    public int[] finalPrices(int[] prices) {
+        //单调栈存数据的下标索引
+        Stack<Integer> stack=new Stack<>();
+        int length=prices.length;
+        for(int i=0;i<prices.length;i++){
+            int num=prices[i];
+            while(!stack.isEmpty()&&num<=prices[stack.peek()]){
+                //出栈，记录索引
+                int index=stack.pop();
+                prices[index]=prices[index]-num;
+            }
+            //索引i入栈
+            stack.push(i);
+        }
+        return prices;
     }
 }
