8c223d015f7f5638ff7dc590bf9b77028278f74a
Jerry <2801987135@qq.com>
2023-12-27 10:25:39 +0800

    链表

---
+++ src/Algorithm/CodeCapriccio/Ch2_LinkedList/ListNode.java
@@ -0,0 +1,9 @@
+package Algorithm.CodeCapriccio.Ch2_LinkedList;
+
+public class ListNode {
+    int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
