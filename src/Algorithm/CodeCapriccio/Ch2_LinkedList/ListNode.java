7e07472ac29fe90453bb2bfc6fa7e446864419b0
Jerry <2801987135@qq.com>
2024-01-13 23:57:29 +0800

    添加add函数

--- src/Algorithm/CodeCapriccio/Ch2_LinkedList/ListNode.java
+++ src/Algorithm/CodeCapriccio/Ch2_LinkedList/ListNode.java
@@ -4,9 +4,16 @@ package Algorithm.CodeCapriccio.Ch2_LinkedList;
  * @author jmjtc
  */
 public class ListNode {
-    int val;
+     int val;
      ListNode next;
      ListNode() {}
      ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     public void add(int val){
+         ListNode temp=this;
+         while(temp.next!=null){
+             temp=temp.next;
+         }
+         temp.next=new ListNode(val);
+     }
 }
