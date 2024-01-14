c9afb0750946ddd31b135bddb53227beb82c0445
Jerry <2801987135@qq.com>
2024-01-14 16:58:45 +0800

    有效字母异位词

---
+++ src/Algorithm/CodeCapriccio/Ch2_LinkedList/Problem242.java
@@ -0,0 +1,23 @@
+package Algorithm.CodeCapriccio.Ch2_LinkedList;
+
+/**
+ * @author jmjtc
+ */
+public class Problem242 {
+    public boolean isAnagram(String s, String t) {
+        int[] a=new int [26];
+        int[] b=new int [26];
+        for(int i=0;i<s.length();i++){
+            a[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            b[t.charAt(i)-'a']++;
+        }
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
