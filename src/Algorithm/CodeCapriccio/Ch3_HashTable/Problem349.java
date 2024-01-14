c85bf75e470f85e3a3d80da155c1a6f769914cd8
Jerry <2801987135@qq.com>
2024-01-14 19:18:51 +0800

    两个数组的交集

---
+++ src/Algorithm/CodeCapriccio/Ch3_HashTable/Problem349.java
@@ -0,0 +1,30 @@
+package Algorithm.CodeCapriccio.Ch3_HashTable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author jmjtc
+ */
+public class Problem349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        List<Integer> res=new ArrayList<>();
+        Set<Integer> visited=new HashSet<>();
+        for (int j : nums1) {
+            visited.add(j);
+        }
+        for (int j : nums2) {
+            if (visited.contains(j)&& !res.contains(j)) {
+                res.add(j);
+            }
+        }
+        int[] ans=new int[res.size()];
+        int i=0;
+        for(int list:res){
+            ans[i++]=list;
+        }
+        return ans;
+    }
+}
