2123fe1e3f2270e6e0248f4854d5af7cef6df27d
Jerry <2801987135@qq.com>
2024-01-25 17:47:17 +0800

    反转字符串

---
+++ src/Algorithm/CodeCapriccio/Ch4_CharacterString/Problem344.java
@@ -0,0 +1,21 @@
+package Algorithm.CodeCapriccio.Ch4_CharacterString;
+
+/**
+ * @author jmjtc
+ */
+public class Problem344 {
+    public static void main(String[] args) {
+
+    }
+    public void reverseString(char[] s) {
+        char temp;
+        int l=0,r=s.length-1;
+        while(l<r){
+            temp=s[l];
+            s[l]=s[r];
+            s[r]=temp;
+            l++;
+            r--;
+        }
+    }
+}
