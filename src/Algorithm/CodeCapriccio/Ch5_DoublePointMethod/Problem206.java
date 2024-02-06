bcb9d350eb83918e21137f855c16c1a636fa4983
Jerry <2801987135@qq.com>
2024-02-06 14:57:07 +0800

    反转链表

---
+++ src/Algorithm/CodeCapriccio/Ch5_DoublePointMethod/Problem206.java
@@ -0,0 +1,47 @@
+package Algorithm.CodeCapriccio.Ch5_DoublePointMethod;
+
+
+import java.util.List;
+
+/**
+ * @author jmjtc
+ */
+public class Problem206 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseList(ListNode head){
+        ListNode temp;
+        ListNode currentNode=head;
+        ListNode prev=null;
+        while(currentNode!=null){
+            temp=currentNode.next;
+            currentNode.next=prev;
+            prev=currentNode;
+            currentNode=temp;
+        }
+        return prev;
+    }
+
+    //递归法
+    public ListNode reverseList1(ListNode head){
+        return reverse(null,head);
+    }
+    public ListNode reverse(ListNode prev,ListNode currentNode){
+        if(currentNode==null){
+            return prev;
+        }
+        ListNode temp=currentNode.next;
+        currentNode.next=prev;
+        return reverse(currentNode,temp);
+    }
+}
+
+class ListNode {
+      int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
