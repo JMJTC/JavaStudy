951f799a18b7cc1cf5a73929abf55d9c2e37ab11
Jerry <2801987135@qq.com>
2024-02-06 15:00:01 +0800

    反转链表

--- src/Algorithm/CodeCapriccio/Ch5_DoublePointMethod/Problem206.java
+++ src/Algorithm/CodeCapriccio/Ch5_DoublePointMethod/Problem206.java
@@ -38,10 +38,3 @@ public class Problem206 {
     }
 }
 
-class ListNode {
-      int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
