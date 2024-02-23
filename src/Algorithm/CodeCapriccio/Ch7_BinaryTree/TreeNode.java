423a9e67dd44dc0d2b8b21ace625ceb27095ac84
Jerry <2801987135@qq.com>
2024-02-23 22:24:33 +0800

    二叉树

---
+++ src/Algorithm/CodeCapriccio/Ch7_BinaryTree/TreeNode.java
@@ -0,0 +1,18 @@
+package Algorithm.CodeCapriccio.Ch7_BinaryTree;
+
+/**
+ * @author jmjtc
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+        this.val=val;
+    }
+    TreeNode(int val,TreeNode left,TreeNode right){
+        this.val=val;
+        this.left=left;
+        this.right=right;
+    }
+}
