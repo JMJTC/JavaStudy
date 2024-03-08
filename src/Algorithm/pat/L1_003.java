dc38c4a3e8a7bd635cf4ba72337d1487809ca811
JMJTC <2801987135@qq.com>
2024-03-08 14:56:23 +0800

    个位数统计

---
+++ src/Algorithm/pat/L1_003.java
@@ -0,0 +1,25 @@
+package Algorithm.pat;
+
+import java.util.Scanner;
+
+/**
+ * @author jmjtc
+ */
+public class L1_003 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str= sc.nextLine();
+
+        int[] data=new int[10];
+        for(int i=0;i<str.length();i++){
+            data[str.charAt(i)-'0']++;
+        }
+
+        //打印
+        for(int i=0;i<10;i++){
+            if(data[i]!=0){
+                System.out.println(i+":"+data[i]);
+            }
+        }
+    }
+}
