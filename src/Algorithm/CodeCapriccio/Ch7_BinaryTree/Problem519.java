aabcae1c3d08533798cead989dbe49fc0fe30340
JMJTC <2801987135@qq.com>
2024-03-12 19:36:03 +0800

    在每个树行中找最大值

---
+++ src/Algorithm/CodeCapriccio/Ch7_BinaryTree/Problem519.java
@@ -0,0 +1,41 @@
+package Algorithm.CodeCapriccio.Ch7_BinaryTree;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author jmjtc
+ */
+public class Problem519 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        if(root==null) {
+            return ans;
+        }
+        //层序遍历
+        Deque<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size= queue.size();
+            int max=Integer.MIN_VALUE;
+            for(int i=0;i<size;i++){
+                TreeNode node=queue.poll();
+                max=Math.max(max,node.val);
+                if(node.left!=null){
+                    queue.offer(node.left);
+                }
+                if(node.right!=null){
+                    queue.offer(node.right);
+                }
+            }
+            ans.add(max);
+        }
+        return ans;
+    }
+}
