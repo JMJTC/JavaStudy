4d77df633d320a0c34d7be20871856e9af496a6c
JMJTC <2801987135@qq.com>
2024-03-16 14:47:36 +0800

    斐波那契数

---
+++ src/Algorithm/CodeCapriccio/Ch10_DynamicPlanning/Problem509.java
@@ -0,0 +1,27 @@
+package Algorithm.CodeCapriccio.Ch10_DynamicPlanning;
+
+/**
+ * @author jmjtc
+ */
+public class Problem509 {
+    public static void main(String[] args) {
+
+    }
+
+    public int fib(int n) {
+        int ans=0;
+        int a0=0,a1=1;
+        if(n==1){
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+        for(int i=0;i<n;i++){
+            ans=a0+a1;
+            a0=a1;
+            a1=ans;
+        }
+        return ans;
+    }
+}
