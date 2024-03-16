069b26024a0342aa719f1cf44dd67804bef104c3
JMJTC <2801987135@qq.com>
2024-03-16 15:51:49 +0800

    不同路径

--- src/Algorithm/CodeCapriccio/Ch10_DynamicProgramming/Problem62.java
+++ src/Algorithm/CodeCapriccio/Ch10_DynamicProgramming/Problem62.java
@@ -22,4 +22,19 @@ public class Problem62 {
         }
         return dp[m-1][n-1];//返回最终结果
     }
+
+    //组合数的计算方法
+    public int uniquePaths1(int m, int n) {
+        // 计算 C(m-1, n-1)
+        long ans = 1;
+        // 优化空间，使用两个变量分别记录 x 和 y
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            // 计算 ans * x / y，同时保持 ans 的结果为整数
+            ans = ans * x / y;
+        }
+        // 返回计算结果，注意强转为 int 类型
+        return (int) ans;
+    }
+
+
 }
