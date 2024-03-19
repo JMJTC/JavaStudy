113f3ef3e320fed0c1659b3b287f8f96ec77a3b8
JMJTC <2801987135@qq.com>
2024-03-19 12:50:48 +0800

    组合总和

---
+++ src/Algorithm/CodeCapriccio/Ch8_BacktrackingAlgorithm/Problem39.java
@@ -0,0 +1,42 @@
+package Algorithm.CodeCapriccio.Ch8_BacktrackingAlgorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jmjtc
+ * 组合总和
+ */
+public class Problem39 {
+    private List<List<Integer>> ans=new ArrayList<>();
+    private List<Integer> path=new ArrayList<>();
+    private int sum=0;
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        backTracking(candidates,target,0);
+        return ans;
+    }
+
+    public void backTracking(int[] nums,int target,int index){
+        if(sum==target){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        if(sum>target){
+            return;
+        }
+        for(int i=index;i<nums.length;i++){
+            sum+=nums[i];
+            path.add(nums[i]);
+            backTracking(nums,target,index);
+            //更新index
+            index++;
+            //回溯
+            sum-=nums[i];
+            path.remove(path.size()-1);
+        }
+    }
+}
