7be9cd83bcc82669c9ac391c88439711f481bbfa
JMJTC <2801987135@qq.com>
2024-04-29 23:27:07 +0800

    SnapshotArray

---
+++ src/Algorithm/LeetCode/SnapshotArray.java
@@ -0,0 +1,27 @@
+package Algorithm.LeetCode;
+
+/**
+ * @author jmjtc
+ */
+public class SnapshotArray {
+    private int[] arr;
+    private int[] snap=new int[5001];
+    private int count=0;
+    public SnapshotArray(int length) {
+        arr=new int[length];
+    }
+
+    public void set(int index, int val) {
+        arr[index]=val;
+    }
+
+    public int snap() {
+        snap[count]=arr[count];
+        count++;
+        return count-1;
+    }
+
+    public int get(int index, int snap_id) {
+
+    }
+}
