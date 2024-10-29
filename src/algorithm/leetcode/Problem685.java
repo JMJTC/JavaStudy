9a995b38136748742e4cfa934d94a3354420e0c0
JMJTC <2801987135@qq.com>
2024-10-29 13:34:48 +0800

    并查集

---
+++ src/algorithm/leetcode/Problem685.java
@@ -0,0 +1,19 @@
+package algorithm.leetcode;
+
+/**
+ * @Author: jmjtc
+ * @CreateTime: 2024-10-28
+ * @Description: <a href="https://leetcode.cn/problems/redundant-connection-ii/description/">冗余连接II</a><
+ * @Version: 1.0
+ */
+public class Problem685 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] findRedundantDirectedConnection(int[][] edges) {
+        int[] ans=new int[2];
+
+        return ans;
+    }
+}
