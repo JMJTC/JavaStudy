6287cb6869684b245f212412ba70ca62f452c0ff
JMJTC <2801987135@qq.com>
2024-11-04 10:25:02 +0800

    平方数之和

--- src/algorithm/leetcode/Problem633.java
+++ src/algorithm/leetcode/Problem633.java
@@ -18,13 +18,41 @@ public class Problem633 {
 
     public boolean judgeSquareSum(int c) {
         Set<Integer> set = new HashSet<>();
-        set.add(0);
-        for (int i = 1; i < Math.sqrt(Integer.MAX_VALUE); i++) {
+        int m=(int)Math.sqrt(c);
+        for (int i = 0; i <=m; i++) {
             set.add(i * i);
         }
         for (Integer i : set) {
             if (set.contains(c - i)) {
                 return true;
+            }
+        }
+        return false;
+    }
+
+    //枚举
+    public boolean judgeSquareSum1(int c) {
+        for (int a = 0; a * a <= c / 2; a++) {
+            int b = (int) Math.sqrt(c - a * a);
+            if (a * a + b * b == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //双指针
+    public boolean judgeSquareSum2(int c) {
+        int a = 0;
+        int b = (int) Math.sqrt(c);
+        while (a <= b) {
+            if (a * a == c - b * b) { // 避免溢出
+                return true;
+            }
+            if (a * a < c - b * b) {
+                a++;
+            } else {
+                b--;
             }
         }
         return false;
