3842a5908d0cfa2ea497005085893d7a73e16a00
JMJTC <2801987135@qq.com>
2024-11-18 23:29:20 +0800

    状态模式

---
+++ src/designpattern/kama/ch20/Main.java
@@ -0,0 +1,13 @@
+package designpattern.kama.ch20;
+
+/**
+ * @Author: jmjtc
+ * @CreateTime: 2024-11-18
+ * @Description: 状态模式
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
