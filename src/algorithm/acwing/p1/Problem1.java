a6c574c7dc2501a3e4e4eead961869ee72d211d0
JMJTC <2801987135@qq.com>
2025-02-03 14:49:58 +0800

    A+B

---
+++ src/algorithm/acwing/p1/Problem1.java
@@ -0,0 +1,30 @@
+package algorithm.acwing.p1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+/**
+ * @Author: jmjtc
+ * @CreateTime: 2025-02-03
+ * @Description: A+B
+ * @Version: 1.0
+ */
+public class Problem1 {
+    public static void main(String[] args) throws Exception{
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(a + b);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] s = br.readLine().split(" ");
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        bw.write(a + b + "\n");
+        bw.flush();
+    }
+}
