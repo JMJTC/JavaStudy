9b0c0a4884ee8a2548d1711d49ea71ec21a48449
JMJTC <2801987135@qq.com>
2025-05-05 11:16:23 +0800

    offer150 跳跃游戏 贪心

---
+++ src/algorithm/offer150/ch01/Problem09.java
@@ -0,0 +1,27 @@
+package algorithm.offer150.ch01;
+
+/**
+ * @Author: jmjtc
+ * @CreateTime: 2025-05-05
+ * @Description: 55. 跳跃游戏
+ * @Version: 1.0
+ */
+public class Problem09 {
+    public static void main(String[] args) {
+
+    }
+
+    // 贪心
+    public boolean canJump(int[] nums) {
+        int length = nums.length;
+        int distance = nums[0];
+        for (int i = 1; i <= distance && i < length; i++){
+            distance = Math.max(distance, i + nums[i]);
+            if (distance >= length - 1){
+                return true;
+            }
+        }
+        return distance >= length - 1;
+    }
+
+}
