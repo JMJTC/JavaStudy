6c051d41a5544f2acb1308e7b933a1c39b8f0a31
JMJTC <2801987135@qq.com>
2025-06-03 20:58:11 +0800

    【offer150】【ones:2】【ch01-14 134. 加油站 贪心 添加注释】

--- src/algorithm/offer150/ch01/Problem14.java
+++ src/algorithm/offer150/ch01/Problem14.java
@@ -11,21 +11,35 @@ public class Problem14 {
 
     }
 
+    /**
+     * 判断能否完成环形路线
+     *
+     * @param gas  每个加油站的汽油量
+     * @param cost 每个加油站到下一个加油站所需的汽油量
+     * @return 如果可以完成环形路线，返回起始加油站的索引；否则返回-1
+     */
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int length = gas.length;
-        int sum = 0;
-        int min = 0;
-        int ans = 0;
+        int length = gas.length; // 获取加油站的数量
+        int sum = 0; // 用于记录总的汽油量减去总的成本的差值
+        int min = 0; // 用于记录当前遍历过程中汽油量减去成本的最小差值
+        int ans = 0; // 用于记录可能的起始加油站索引
+
+        // 遍历所有加油站
         for (int i = 0; i < length; i++) {
-            sum += gas[i] - cost[i];
+            sum += gas[i] - cost[i]; // 累加当前加油站的汽油量减去成本的差值
+            // 如果当前累加的差值小于之前记录的最小差值
             if (sum < min) {
-                min = sum;
-                ans = i + 1;
+                min = sum; // 更新最小差值
+                ans = i + 1; // 更新可能的起始加油站索引为当前索引加1
             }
         }
+
+        // 如果总的汽油量减去总的成本小于0，说明无法完成环形路线
         if (sum < 0) {
-            return -1;
+            return -1; // 返回-1表示无法完成环形路线
         }
+
+        // 返回可能的起始加油站索引，取模是为了处理环形结构
         return ans % length;
     }
 }
